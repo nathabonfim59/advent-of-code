d4b143cced7b14db264164b4a885b4452d5f957d
nathabonfim59 <dev@nathabonfim59.com>
2025-12-01 07:40:27 -0300

    feat(2015/c): better debugger

--- c/2015/day-02/part1.c
+++ c/2015/day-02/part1.c
@@ -1,5 +1,8 @@
 #include "../lib/file.h"
 #include "../lib/debug.h"
+#include "../lib/text.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 struct Dimentions {
 	int length;
@@ -11,21 +14,28 @@ struct Dimentions parseLine(char *line);
 
 int main() {
 	struct File file = openFile("sample.txt");
-
 	char *line = getLine(&file);
-
 	struct Dimentions dm = parseLine(line);
 
-	dd_struct(dm, length);
+	dd_struct(dm, length, width, height);
 
 	return 0;
 }
 
 struct Dimentions parseLine(char *line) {
+	char **parts = NULL;
+	int count = 0;
+
+	split(line, 'x', &parts, &count);
+
+	if (count != 3) {
+		printf("Invalid line format: %s\n", line);
+	}
+
 	struct Dimentions dimentions = {
-		.height = 0,
-		.length = 1,
-		.width = 0
+		.length = atoi(parts[0]),
+		.width = atoi(parts[1]),
+		.height = atoi(parts[2]),
 	};
 
 	return dimentions;
